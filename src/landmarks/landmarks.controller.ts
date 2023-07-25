4a07b3f0b3148537f6671ad11c068ac7b8c4871d
Your Name <leelouncao@gmail.com>
2023-07-25 15:40:52 +0900

    🎨 Chore: 엔티티 추가

--- src/landmarks/landmarks.controller.ts
+++ src/landmarks/landmarks.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Post, Get, Param, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { Controller, Get, Param, UseInterceptors, UploadedFile } from '@nestjs/common';
 import { LandmarkService } from './landmarks.service';
-import { Landmark } from '@prisma/client';
 import { GetLandmarkDto } from './dto/landmark.request.dto';
 import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import LandmarkResponse from 'src/docs/contents.swagger';
@@ -12,6 +11,23 @@ import { LandmarkResponseDto } from './dto/landmark.response.dto';
 @Controller('landmark')
 export class LandmarkController {
   constructor(private readonly landmarkService: LandmarkService) {}
+
+  @Get(':name')
+  @ApiParam({ name: 'name', required: true, description: 'The name of the landmark' })
+  @ApiOperation({ summary: 'Get a landmark by name' })
+  @ApiResponse(LandmarkResponse)
+  async getLandmark(@Param() getLandmarkDto: GetLandmarkDto): 
+    Promise<{ landmark: LandmarkResponseDto, nearByLandmarks: LandmarkResponseDto[] }> {
+
+    //해당 랜드마크 정보 추출
+    const landmark : LandmarkResponseDto = await this.landmarkService.getLandmarkByName(getLandmarkDto)
+
+    //근처 랜드마크 리스트 추출
+    const nearByLandmarks =  await this.landmarkService.getNearByLandmarksByArea(landmark.areaId);
+    
+
+    return { landmark, nearByLandmarks}
+  }
 
   // @Post('upload')
   // @ApiFile()
@@ -34,21 +50,4 @@ export class LandmarkController {
   //     imagePath
   //   };
   // }
-
-  @Get(':name')
-  @ApiParam({ name: 'name', required: true, description: 'The name of the landmark' })
-  @ApiOperation({ summary: 'Get a landmark by name' })
-  @ApiResponse(LandmarkResponse)
-  async getLandmark(@Param() getLandmarkDto: GetLandmarkDto): 
-    Promise<{ landmark: LandmarkResponseDto, nearByLandmarks: LandmarkResponseDto[] }> {
-
-    //해당 랜드마크 정보 추출
-    const landmark : LandmarkResponseDto = await this.landmarkService.getLandmarkByName(getLandmarkDto)
-
-    //근처 랜드마크 리스트 추출
-    const nearByLandmarks =  await this.landmarkService.getNearByLandmarksByArea(landmark.areaId);
-    
-
-    return { landmark, nearByLandmarks}
-  }
 }
--- src/landmarks/landmarks.service.ts
+++ src/landmarks/landmarks.service.ts
@@ -1,4 +1,3 @@
-import { Landmark } from '@prisma/client';
 import { Injectable } from '@nestjs/common';
 import { LandmarkRepository } from './landmarks.repository';
 import { GetLandmarkDto } from './dto/landmark.request.dto';
@@ -14,17 +13,6 @@ export class LandmarkService {
     private readonly s3Service: S3Service,
   ) {}
  
-  // async uploadImage(fileNameKey: string, fileBuffer: Buffer, fileName: string ): Promise<Landmark> {
-  //   const filePath = await this.s3Service.uploadFile(fileNameKey, fileBuffer);
-
-  //   const landmark = await this.landmarkRepo.uploadImage(fileName);
-  //   if (!landmark) {
-  //     throw new Error('Landmark not found');
-  //   }
-  //   return this.landmarkRepo.updateImagePathById(landmark.id, filePath);
-  // }
-
-
   //해당 랜드마크 정보 추출 (이름으로 검색)
   async getLandmarkByName(getLandmarkDto: GetLandmarkDto): Promise<LandmarkResponseDto> {
     let landmark = await this.landmarkRepo.findLandmarkByName(getLandmarkDto);
@@ -35,9 +23,7 @@ export class LandmarkService {
     const imagePath = getImagePath(landmark.imagePath);
     landmark.imagePath = imagePath;
 
-    landmark = plainToClass(LandmarkResponseDto, landmark);
-
-    return landmark
+    return plainToClass(LandmarkResponseDto, landmark);
   }
 
   //주변 랜드마크 정보 추출 (지역ID로 검색 - 상위 5개)
@@ -53,12 +39,17 @@ export class LandmarkService {
       ...landmark,
       imagePath: getImagePath(landmark.imagePath),
     }));
-
-    //할당
-    const nearByLandmarks = updatedLandmarks.map(landmark => plainToClass(LandmarkResponseDto, landmark));
   
-    return nearByLandmarks;
+    return updatedLandmarks.map(landmark => plainToClass(LandmarkResponseDto, landmark));
   }
 
+  // async uploadImage(fileNameKey: string, fileBuffer: Buffer, fileName: string ): Promise<Landmark> {
+  //   const filePath = await this.s3Service.uploadFile(fileNameKey, fileBuffer);
 
+  //   const landmark = await this.landmarkRepo.uploadImage(fileName);
+  //   if (!landmark) {
+  //     throw new Error('Landmark not found');
+  //   }
+  //   return this.landmarkRepo.updateImagePathById(landmark.id, filePath);
+  // }
 }
