55e9f9d9e25184c7cd01cac0b901b1feda9e2b25
nayeon <wkdskdus@gmail.com>
2023-07-25 22:15:53 +0900

    ✨ Feat: User & Bookmark 모델 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LandmarksModule } from './landmarks/landmarks.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     LandmarksModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
