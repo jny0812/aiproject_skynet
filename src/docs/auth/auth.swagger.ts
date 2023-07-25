f1e1184ef25ab4757e7627cc210741d5b268563c
nayeon <wkdskdus@gmail.com>
2023-07-26 00:43:39 +0900

    ✨ Feat: Auth Response Dto 완료

---
+++ src/docs/auth/auth.swagger.ts
@@ -0,0 +1,14 @@
+// import { Auth } from "src/landmarks/dto/landmark.response.dto"
+
+// const LandmarkResponse = {
+//     status: 200,
+//     description: `
+//     제공 정보: 
+//      \`name\`: 제목 
+//      \`address\`: 주소 
+//      \`imagePath\`: 이미지 Path 
+//      \`nearByLandmarks\` : 근처 랜드마크 리스트
+//     `,
+//     type: LandmarkResponseDto,
+// }
+// export default LandmarkResponse
