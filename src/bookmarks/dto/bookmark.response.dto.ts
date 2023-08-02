28a6e3b5e101990ee9a908d8ced649a657e57470
nayeon <wkdskdus@gmail.com>
2023-08-02 09:28:04 +0900

    Feat: 마이페이지에 북마크 리스트 추가

--- src/bookmarks/dto/bookmark.response.dto.ts
+++ src/bookmarks/dto/bookmark.response.dto.ts
@@ -1,70 +1,42 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { Area, Bookmark, Landmark } from "@prisma/client";
-import { LandmarkEntity } from "src/landmarks/landmark.entity";
 
 export class ResponseBookmarkDto {
-  @ApiProperty({ example: 1, description: 'The id of the bookmark' })
+  @ApiProperty({ example: 1, description: "The id of the bookmark" })
   id: number;
 
-  @ApiProperty({ example: '2023-07-31T12:00:00Z', description: '북마크 생성시간' })
-  createdAt: Date;
-
-  @ApiProperty({ example: '6230c68b-3e08-484d-818b-5bfa966d5d77', description: '북마크의 유저ID' })
+  @ApiProperty({
+    example: "6230c68b-3e08-484d-818b-5bfa966d5d77",
+    description: "북마크의 유저ID",
+  })
   userId: string;
 
   @ApiProperty({
-    example: {
-      address: '서울특별시 종로구 종로 1(종로1가)',
-      name: '(주)교보문고',
-      imagePath: '(주)교보문고_001_34728823.jpg',
-      area: { areaId: 1 },
-    },
-    description: 'The landmark associated with the bookmark',
+    example: "강남",
+    description: "자치구",
   })
-  landmark: {
-    address: string;
-    name: string;
-    imagePath: string;
-    area: {
-      areaId: number;
-    };
-  };
+  siGu: string;
 
-  constructor(bookmark: Bookmark & { landmark: Landmark & { area: Area } }) {
-    this.id = bookmark.id;
-    this.createdAt = bookmark.createdAt;
-    this.userId = bookmark.userId;  // Add this line
-    this.landmark = {
-      address: bookmark.landmark.address,
-      name: bookmark.landmark.name,
-      imagePath: bookmark.landmark.imagePath,
-      area: {
-        areaId: bookmark.landmark.area.id,
-      },
-    };
-  }
+  @ApiProperty({
+    example: "150년 수령 느티나무_001_34844496.jpg",
+    description: "이미지 URL",
+  })
+  imagePath: string;
+
+  @ApiProperty({
+    example: "150년수령느티나무",
+    description: "장소명",
+  })
+  name: string;
+
+  @ApiProperty({
+    example: "서울특별시 양천구 목4동 760-28",
+    description: "주소",
+  })
+  address: string;
+
+  @ApiProperty({
+    example: "2023-07-31T12:00:00Z",
+    description: "북마크 생성시간",
+  })
+  createdAt: Date;
 }
-
-//import { Landmark } from "./src/landmark/landmark.entity";
-
-// export class GetBookmarkResponseDto {
-//   @ApiProperty({ example: 1, description: "The id of the bookmark" })
-//   id: number;
-
-//   @ApiProperty({
-//     example: {
-//       id: 1,
-//       name: "Landmark name",
-//       address: "Landmark address",
-//       imagePath: "Landmark image path",
-//       area: {
-//         id: 1,
-//         name: "Area name",
-//       },
-//     },
-//     description: "The landmark associated with the bookmark",
-//     type: LandmarkEntity,
-//   })
-//   landmark: Landmark;
-// }
-
